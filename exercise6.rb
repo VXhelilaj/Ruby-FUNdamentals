9644f523c587af70a99f22d32896a069357ca4b6
viktor <v.xhelilaj@gmail.com>
2016-08-10 15:14:09 -0400

    actually added new element

--- exercise6.rb
+++ exercise6.rb
@@ -1,1 +1,3 @@
 element_list = ["water", "fire", "ice", "earth", "air", "metal", "shadow", "void", "poison", "lightning", "sound", "blood", "light", "plant", "dust"]
+element_list << "new_element"
+puts element_list
