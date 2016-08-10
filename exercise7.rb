eb912d007a5c38c02c2b21511ba87482edd7a7c1
viktor <v.xhelilaj@gmail.com>
2016-08-10 19:47:29 -0400

    keys names output for cohorts

--- exercise7.rb
+++ exercise7.rb
@@ -8,3 +8,14 @@ students = {
 students.each do |k, v|
   puts ":#{k}, #{v} students"
 end
+
+puts ""
+
+students = {
+  :cohort1 => 34,
+  :cohort2 => 42,
+  :cohort3 => 22,
+  :cohort4 => 43
+}
+
+puts students.keys
