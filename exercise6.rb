28bc79765ffb874dc5fa2f7a7d6f3f365485adaa
viktor <v.xhelilaj@gmail.com>
2016-08-10 15:17:14 -0400

    counted items on the list

--- exercise6.rb
+++ exercise6.rb
@@ -1,3 +1,4 @@
 element_list = ["water", "fire", "ice", "earth", "air", "metal", "shadow", "void", "poison", "lightning", "sound", "blood", "light", "plant", "dust"]
 element_list << "new_element"
 puts element_list
+puts element_list.length
