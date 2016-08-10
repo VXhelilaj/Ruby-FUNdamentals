823f353293f874954f2315178ffbf40730b18553
viktor <v.xhelilaj@gmail.com>
2016-08-10 15:28:59 -0400

    no banans here

--- exercise6.rb
+++ exercise6.rb
@@ -1,4 +1,15 @@
 element_list = ["water", "fire", "ice", "earth", "air", "metal", "shadow", "void", "poison", "lightning", "sound", "blood", "light", "plant", "dust"]
+
 element_list << "new_element"
+
 puts element_list
+
 puts element_list.length
+
+element_list.include?('Banana')
+
+if element_list.include?('Banana') == false
+  puts "No bananas on this list hombre"
+else
+  puts "Where did that banana come from?"
+end
